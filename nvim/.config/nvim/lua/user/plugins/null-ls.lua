a1e894d0efd4f4fc331852d5c9ef6b6078f772ae
Christian Scano <christian.scano@pluribus-one.it>
2023-08-04 11:58:54 +0200

    Added shellcheck

--- nvim/.config/nvim/lua/user/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/user/plugins/null-ls.lua
@@ -1,30 +1,31 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  opts = function(_, opts)
-    local null_ls = require "null-ls"
-    opts.sources = {
-      -- lua formatter
-      null_ls.builtins.formatting.stylua,
+	"jose-elias-alvarez/null-ls.nvim",
+	opts = function(_, opts)
+		local null_ls = require("null-ls")
+		opts.sources = {
+			-- lua formatter
+			null_ls.builtins.formatting.stylua,
 
-      -- web stuff formatter
-      null_ls.builtins.formatting.prettier,
-      -- python formatter/linter
-      null_ls.builtins.formatting.black,
-      null_ls.builtins.formatting.isort,
-      null_ls.builtins.diagnostics.flake8,
+			-- web stuff formatter
+			null_ls.builtins.formatting.prettier,
+			-- python formatter/linter
+			null_ls.builtins.formatting.black,
+			null_ls.builtins.formatting.isort,
+			null_ls.builtins.diagnostics.flake8,
 
-      -- c/c++ formatter
-      null_ls.builtins.formatting.clang_format,
+			-- c/c++ formatter
+			null_ls.builtins.formatting.clang_format,
 
-      -- bash formatter
-      null_ls.builtins.formatting.shfmt.with {
-        args = { "-i", "2" },
-      },
+			-- bash stuff
+			null_ls.builtins.formatting.shfmt.with({
+				args = { "-i", "2" },
+			}),
+			null_ls.builtins.diagnostics.shellcheck,
 
-      -- It has been disabled since required `luarocks` in
-      -- the local enviroment.
-      --null_ls.builtins.diagnostics.luacheck,
-    }
-    return opts
-  end,
+			-- It has been disabled since required `luarocks` in
+			-- the local enviroment.
+			--null_ls.builtins.diagnostics.luacheck,
+		}
+		return opts
+	end,
 }

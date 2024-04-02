fa92e03a9a7389f218eb198908823f2579c70d2c
Christian Scano <cris.scano@outlook.it>
2024-04-02 19:39:34 +0200

    Added scripts

---
+++ scripts/looking-glass.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#  _                _    _                ____ _
+# | |    ___   ___ | | _(_)_ __   __ _   / ___| | __ _ ___ ___
+# | |   / _ \ / _ \| |/ / | '_ \ / _` | | |  _| |/ _` / __/ __|
+# | |__| (_) | (_) |   <| | | | | (_| | | |_| | | (_| \__ \__ \
+# |_____\___/ \___/|_|\_\_|_| |_|\__, |  \____|_|\__,_|___/___/
+#                                |___/
+#
+# -----------------------------------------------------
+
+tmp=$(virsh --connect qemu:///system list | grep " win11-gpu " | awk '{ print $3}')
+
+if [ "$tmp" == "x" ] || [ "$tmp" != "xrunning" ]; then
+  virsh --connect qemu:///system start win11
+  echo "Virtual Machine win11 is starting..."
+  sleep 3
+fi
+
+looking-glass-client -m 97 &
+exit

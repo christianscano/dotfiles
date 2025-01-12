7beaf8e14bf3e859ff89deba8acd2689bfc9a0c7
christianscano <cris.scano@outlook.it>
2025-01-12 18:25:05 +0100

    Working on install script

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/zsh
 
+# -----------------
+# General functions
+# -----------------
+
+_seekConfirmation_() {
+  # v1.0.0
+  echo "${bold}$@${reset}"
+  while true; do
+    read -r -p " (y/n) " yn
+    case $yn in
+      [Yy]*) return 0 ;;
+      [Nn]*) return 1 ;;
+      *) echo "Pleas answer yes or no" ;;
+    esac
+  done
+}
+
 # Install xcode tools
 echo "Installing xcode tools..."
 xcode-select --install
@@ -9,51 +26,37 @@ echo "Install Brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew analytics off
 
-# Taps
-echo "Tapping Brew..."
-brew tap FelixKratz/formulae
-brew tap koekeishiya/formulae
-
-# Formulae
-echo "Installing Brew Formulae..."
-
-## Base
-brew install borders
-brew install wget
-brew install watch
-brew install ripgrep
-brew install jq
-brew install grep
-brew install sketchybar
-brew install stow
-
-# Dev
-brew install node
-brew install php
-brew install ruby
-brew install rust
-
-## Research
-brew install graphviz
-
-## Tools
-brew install feroxbuster
-brew install ffuf
-brew install awscli
-brew install jython
-brew install nmap
-
-## Terminal
-brew install neovim
-brew install tmux
-
-# Casks
-
+# --------------
 # macOS settings
-echo ""
-defaults write -g NSWindowShouldDragOnGesture -bool true
-defaults write -g AppleShowAllExtensions -bool true
+# --------------
+echo "[+] MacOS Settings..."
+
+# Keyboard
 defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 15
+
+# Dock
+defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock tilesize -int 38
+defaults write com.apple.dock orientation -string "left"
+
+# Finder
+defaults write -g AppleShowAllExtensions -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Screenshot
+defaults write com.apple.screencapture location -string "${HOME}/Downloads"
+
+# Restart applications
+killall Dock
+killall Finder
+
+#
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
+
+

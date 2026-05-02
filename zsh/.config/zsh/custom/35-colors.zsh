fbadc107a16ac99064e582d6bf6f6d9329bcd466
christianscano <cris.scano@outlook.it>
2026-05-02 21:56:12 +0200

    Disabled colors

--- zsh/.config/zsh/custom/35-colors.zsh
+++ zsh/.config/zsh/custom/35-colors.zsh
@@ -1,20 +1,20 @@
 # # Catppuccin Mocha CLI colors
 
-export EZA_CONFIG_DIR="$HOME/.config/eza"
+# export EZA_CONFIG_DIR="$HOME/.config/eza"
 
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=103'
+# ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=103'
 
-typeset -A ZSH_HIGHLIGHT_STYLES
-ZSH_HIGHLIGHT_STYLES[command]='fg=111'
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=111'
-ZSH_HIGHLIGHT_STYLES[function]='fg=111'
-ZSH_HIGHLIGHT_STYLES[alias]='fg=111'
-ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=183'
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=223'
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=223'
-ZSH_HIGHLIGHT_STYLES[path]='fg=116,underline'
-ZSH_HIGHLIGHT_STYLES[path_pathseparator]='fg=103'
-ZSH_HIGHLIGHT_STYLES[globbing]='fg=183'
-ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=183'
-ZSH_HIGHLIGHT_STYLES[comment]='fg=103'
-ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=210,bold'
+# typeset -A ZSH_HIGHLIGHT_STYLES
+# ZSH_HIGHLIGHT_STYLES[command]='fg=111'
+# ZSH_HIGHLIGHT_STYLES[builtin]='fg=111'
+# ZSH_HIGHLIGHT_STYLES[function]='fg=111'
+# ZSH_HIGHLIGHT_STYLES[alias]='fg=111'
+# ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=183'
+# ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=223'
+# ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=223'
+# ZSH_HIGHLIGHT_STYLES[path]='fg=116,underline'
+# ZSH_HIGHLIGHT_STYLES[path_pathseparator]='fg=103'
+# ZSH_HIGHLIGHT_STYLES[globbing]='fg=183'
+# ZSH_HIGHLIGHT_STYLES[history-expansion]='fg=183'
+# ZSH_HIGHLIGHT_STYLES[comment]='fg=103'
+# ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=210,bold'

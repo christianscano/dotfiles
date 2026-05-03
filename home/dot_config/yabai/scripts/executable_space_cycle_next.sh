1cbe67f4fe0c33ab8cc9b495761aa2c540510b9a
christianscano <cris.scano@outlook.it>
2026-05-04 00:15:19 +0200

    Minor fix

--- home/dot_config/yabai/scripts/executable_space_cycle_next.sh
+++ home/dot_config/yabai/scripts/executable_space_cycle_next.sh
@@ -6,5 +6,5 @@ last=$(echo "$info" | jq '.[-1]."has-focus"')
 if [[ $last == "false" ]]; then
     yabai -m space --focus next
 else
-    yabai -m space --focus $(echo "$info" | jq '.[0].index')
+    yabai -m space --focus "$(echo "$info" | jq '.[0].index')"
 fi
--- home/dot_config/yabai/scripts/executable_space_cycle_prev.sh
+++ home/dot_config/yabai/scripts/executable_space_cycle_prev.sh
@@ -6,5 +6,5 @@ first=$(echo "$info" | jq '.[0]."has-focus"')
 if [[ $first == "false" ]]; then
     yabai -m space --focus prev
 else
-    yabai -m space --focus $(echo "$info" | jq '.[-1].index')
+    yabai -m space --focus "$(echo "$info" | jq '.[-1].index')"
 fi
